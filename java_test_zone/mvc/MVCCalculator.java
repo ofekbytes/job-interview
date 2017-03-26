3f2d81bfd8d3078a1c5277bdbee2da17fc1a25e9
koskas <ykessler@ISRAKIOSK.Local>
2017-03-26 10:19:13 +0300

    test zone

---
+++ java_test_zone/mvc/MVCCalculator.java
@@ -0,0 +1,20 @@
+package mvc;
+
+
+//The Model performs all the calculations needed
+//and that is it. It doesn't know the View exists
+
+public class MVCCalculator {
+    
+    public static void main(String[] args) {
+    	
+    	CalculatorView theView = new CalculatorView();
+        
+    	CalculatorModel theModel = new CalculatorModel();
+        
+        CalculatorController theController = new CalculatorController(theView,theModel);
+        
+        theView.setVisible(true);
+        
+    }
+}
